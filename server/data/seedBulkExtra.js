11b6d4890cc2cd904b3486adf987feeb5b559133
Dilara Wickramanayake <156522027+dilarawick@users.noreply.github.com>
2026-09-15 12:39:01 +0530

    create seedbulkextra

---
+++ server/data/seedBulkExtra.js
@@ -0,0 +1,4 @@
+import { BULK_BIG } from './seedBulkBig.js';
+import { BULK_BIG2 } from './seedBulkBig2.js';
+import { BULK_FINAL } from './seedBulkFinal.js';
+export const BULK_2026 = [...BULK_BIG, ...BULK_BIG2, ...BULK_FINAL];

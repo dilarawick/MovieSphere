8a7aa00d937f183723c452d72fbc5539300b1f6a
Dilara Wickramanayake <156522027+dilarawick@users.noreply.github.com>
2026-09-15 22:29:44 +0530

    update seedbulk horror

--- server/data/seedBulkHorror1.js
+++ server/data/seedBulkHorror1.js
@@ -22,8 +22,8 @@ export const BULK_HORROR = mk([
 [933260, 'The Substance', 2024, 7.3, ['Horror','Drama'], 'A fading star tries a youth serum. Oscar winner.', 'Demi Moore', 'Coralie Fargeat', 1, 'Stream on Hulu. Rent on Prime Video.', ['hulu']],
 [1114894, 'Nosferatu', 2024, 7.2, ['Horror','Fantasy'], 'Eggers remake of the vampire classic.', 'Bill Skarsgard', 'Robert Eggers', 1, 'Stream on Peacock. Rent on Prime Video.', ['peacock']],
 [1184918, 'Heretic', 2024, 7.0, ['Horror','Thriller'], 'Missionaries trapped by a scholar.', 'Hugh Grant', 'Beck Woods', 0, 'Stream on Max. Rent on Prime Video.', ['max']],
-[1176142, 'Terrifier 3', 2024, 6.9, ['Horror'], 'Art the Clown Christmas massacre.', 'David H Thornton', 'Damien Leone', 1, 'Rent on Prime Video, Apple TV.', []],
-[1072349, 'Longlegs', 2024, 6.7, ['Horror','Crime'], 'FBI agent hunts an occult killer.', 'Nicolas Cage', 'Osgood Perkins', 1, 'Stream on Hulu. Rent on Prime Video.', ['hulu']],
+[1034541, 'Terrifier 3', 2024, 6.9, ['Horror'], 'Art the Clown Christmas massacre.', 'David H Thornton', 'Damien Leone', 1, 'Rent on Prime Video, Apple TV.', []],
+[1226578, 'Longlegs', 2024, 6.7, ['Horror','Crime'], 'FBI agent hunts an occult killer.', 'Nicolas Cage', 'Osgood Perkins', 1, 'Stream on Hulu. Rent on Prime Video.', ['hulu']],
 [1054899, 'A Quiet Place: Day One', 2024, 6.3, ['Horror','Sci-Fi'], 'First invasion day in New York.', 'Lupita Nyongo', 'Michael Sarnoski', 0, 'Stream on Paramount+. Rent on Prime Video.', ['paramount']],
 [1010600, 'The First Omen', 2024, 6.5, ['Horror'], 'Prequel to the Omen curse.', 'Nell Tiger Free', 'Arkasha Stevenson', 0, 'Stream on Hulu. Rent on Prime Video.', ['hulu']],
 ]);
